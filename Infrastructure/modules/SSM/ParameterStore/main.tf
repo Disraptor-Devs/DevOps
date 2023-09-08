db2d53620908a2996a0174cb24af29f1a497a2d6
ShibuleNdhambi <shibule.ndhambi@disraptor.co.za>
2023-09-08 13:10:25 +0200

    updated ssm parameter store config

--- Infrastructure/modules/SSM/ParameterStore/main.tf
+++ Infrastructure/modules/SSM/ParameterStore/main.tf
@@ -15,15 +15,11 @@ resource "aws_ssm_parameter" "secret" {
 
 
 data "aws_ssm_parameter" "getSecret" {
-  count = local.call_get_secret
-
   for_each = toset(var.parameter_values_to_get_by_name)
   name     = each.key
 }
 
 data "aws_ssm_parameters_by_path" "getSecretByPath" {
-  count = local.call_get_secret
-
   for_each = toset(var.parameter_values_to_get_by_path)
   path     = each.key
 }
