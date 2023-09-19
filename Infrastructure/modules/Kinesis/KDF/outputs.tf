90433647f708afd2181e24aeb7f25d7ebafe7a23
ShibuleNdhambi <137506833+ShibuleNdhambi@users.noreply.github.com>
2023-09-19 16:03:48 +0200

    update and refactor KDF module

--- Infrastructure/modules/Kinesis/KDF/outputs.tf
+++ Infrastructure/modules/Kinesis/KDF/outputs.tf
@@ -1,11 +1,11 @@
 output "extended_s3_stream_arn" {
   description = "Output for the delivery stream arn for the extended s3 stream"
-  value       = var.is_s3_conumer ? aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].arn : null
+  value       = var.is_s3_consumer ? aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].arn : null
 }
 
 output "extended_s3_stream_name" {
   description = "Output for the delivery stream name for the extended s3 stream"
-  value       = var.is_s3_conumer ? aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].name : null
+  value       = var.is_s3_consumer ? aws_kinesis_firehose_delivery_stream.extended_s3_stream[0].name : null
 }
 
 output "redshift_stream_arn" {
