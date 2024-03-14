f06adda96c82945851e67250258f5603e58d2c4d
ShibuleNdhambi <137506833+ShibuleNdhambi@users.noreply.github.com>
2024-03-14 21:54:03 +0200

    update terraform files

--- Infrastructure/modules/SSM/ParameterStore/outputs.tf
+++ Infrastructure/modules/SSM/ParameterStore/outputs.tf
@@ -6,6 +6,17 @@ output "ssm_paramater_arn" {
   }
 }
 
+output "ssm_paramater_username" {
+  description = "Output the SSM Parameter username"
+  value = aws_ssm_parameter.secret["Username"].value
+  
+}
+
+output "ssm_paramater_passw" {
+  description = "Output the SSM parameter value for the sake of being imported by other modules"
+  value       = aws_ssm_parameter.secret["Password"].value
+  
+}
 output "ssm_paramter_value" {
   description = "Output the SSM parameter value for the sake of being imported by other modules"
   value       = var.is_get_value ? data.aws_ssm_parameter.getSecret[0].value : null
@@ -13,5 +24,5 @@ output "ssm_paramter_value" {
 
 output "ssm_paramter_value_by_path" {
   description = "Output the SSM parameter value by path provided"
-  value       = var.is_get_value_by_path ? data.aws_ssm_parameters_by_path.getSecretByPath[0].values : null
+  value       = var.is_get_value_by_path ? data.aws_ssm_parameters_by_path.getSecretByPath.values : null
 }
