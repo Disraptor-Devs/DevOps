035fc539b1e0ec280b5c07956f6bc3968bdfe47e
ShibuleNdhambi <137506833+ShibuleNdhambi@users.noreply.github.com>
2024-06-03 10:03:34 +0200

    update api gw module

--- Infrastructure/modules/API-Gateway/outputs.tf
+++ Infrastructure/modules/API-Gateway/outputs.tf
@@ -1,22 +1,21 @@
 output "api_gateway_id" {
   description = "Output the API Gateway id"
-  value       = aws_api_gateway_rest_api.api_gateway[0].id
+  value       = var.is_existing_api_gateway ? aws_api_gateway_rest_api.api_gateway[0].id : data.aws_api_gateway_rest_api.existing_api_gateway[0].id
 }
 
 output "api_gateway_root_resource_id" {
   description = "Output the API Gateway root resource id"
-  value       = aws_api_gateway_rest_api.api_gateway[0].root_resource_id
+  value       = var.is_existing_api_gateway ? aws_api_gateway_rest_api.api_gateway[0].root_resource_id : data.aws_api_gateway_rest_api.existing_api_gateway[0].id
 }
 
 
 output "api_gw_integration_id" {
   description = "Output the API Gateway integration"
-  value       = aws_api_gateway_integration.api_gateway_integration[0].id
-  
+  value       = var.is_api_gw_integration ? aws_api_gateway_integration.api_gateway_integration[0].id : null
 }
 
 output "aws_api_gw_resource_id" {
   description = "Output the API Gateway resource id"
-  value       = aws_api_gateway_resource.api_gateway_resource[0].id
+  value       = var.is_api_gw_resource ? aws_api_gateway_resource.api_gateway_resource[0].id : null
   
 }
