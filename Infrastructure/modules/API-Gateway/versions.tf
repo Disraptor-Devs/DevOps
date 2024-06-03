32b4e23d40207c396d796b98662eb949ea6c83cf
ShibuleNdhambi <137506833+ShibuleNdhambi@users.noreply.github.com>
2024-06-03 10:21:15 +0200

    update api gw module

--- Infrastructure/modules/API-Gateway/versions.tf
+++ Infrastructure/modules/API-Gateway/versions.tf
@@ -8,7 +8,4 @@ terraform {
     }
   }
 
-  # Partial configuration, intentionally left empty. The other settings (e.g., bucket, region) will be
-  # passed in from the terragrunt.hcl file via -backend-config arguments to 'terraform init'
-  backend "s3" {}
 }
